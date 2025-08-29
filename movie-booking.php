72d97d088553cc127ca1f9018365dce123dbbf80
xcCodebreaker <xccodebreaker619@gmail.com>
2025-08-29 17:05:08 +0600

    Added delete feature for Admin Page - Movies List

--- movie-booking.php
+++ movie-booking.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Book a movie
 Description: A very simple movie theatre booking system.
-Version: 1.1
+Version: 1.2
 Author: Abdullah
 */
 
@@ -55,9 +55,18 @@ function smb_admin_menu() {
 }
 add_action("admin_menu", "smb_admin_menu");
 
-// Admin: Manage movies 
+// Admin: Manage movies
 function smb_manage_movies_page() {
     global $wpdb;
+
+    // Delete movie
+    if (isset($_GET['delete_movie'])) {
+        $del_id = intval($_GET['delete_movie']);
+        if ($del_id && isset($_GET['_wpnonce']) && wp_verify_nonce($_GET['_wpnonce'], 'smb_delete_movie_' . $del_id)) {
+            $wpdb->delete($wpdb->prefix . "movies", ["id" => $del_id]);
+            echo "<div class='updated'><p>Movie deleted successfully.</p></div>";
+        }
+    }
 
     // Add movie
     if (isset($_POST['smb_add_movie']) && isset($_POST['_wpnonce']) && wp_verify_nonce($_POST['_wpnonce'], 'smb_add_movie')) {
@@ -80,24 +89,27 @@ function smb_manage_movies_page() {
             <button type='submit' name='smb_add_movie' class='button button-primary'>Add Movie</button>
         </form>
         <h3>Current Movies</h3>";
-    
-        if ($movies) {
-            echo "<table class='widefat fixed'>
-                <thead><tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Showtime</th>
-                </tr></thead><tbody>";
-            foreach ($movies as $m) {
-                echo "<tr>
-                    <td>" . esc_html($m->id) . "</td>
-                    <td>" . esc_html($m->title) . "</td>
-                    <td>" . esc_html($m->showtime) . "</td>
-                </tr>";
-            }
-            echo "</tbody></table>";
-        } else {
-            echo "<p>No movies available yet.</p>";
+
+    if ($movies) {
+        echo "<table class='widefat fixed'>
+            <thead><tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Showtime</th>
+                <th>Action</th>
+            </tr></thead><tbody>";
+        foreach ($movies as $m) {
+            $delete_url = wp_nonce_url(admin_url("admin.php?page=smb_manage_movies&delete_movie={$m->id}"), 'smb_delete_movie_' . $m->id);
+            echo "<tr>
+                <td>" . esc_html($m->id) . "</td>
+                <td>" . esc_html($m->title) . "</td>
+                <td>" . esc_html($m->showtime) . "</td>
+                <td><a href='" . esc_url($delete_url) . "' class='button button-danger' onclick='return confirm(\"Are you sure you want to delete this movie?\");'>Delete</a></td>
+            </tr>";
         }
-        echo "</div>";
+        echo "</tbody></table>";
+    } else {
+        echo "<p>No movies available yet.</p>";
     }
+    echo "</div>";
+}
